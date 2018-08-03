66b78b6e6ab6654f12edd957249ad294bd2f19ab
Grzegorz Laszczyk <glas@softserveinc.com>
2018-08-03 13:36:50 +0200

    [js-01] Add playing sounds

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -11,41 +11,41 @@ const codes = {
     '76': 'L' 
 }
 
-const keys = Array.from(document.getElementsByTagName('kbd'));
-
-getParent = (node) => node.parentElement;
+const findAudio = (id) => document.querySelector(`audio[data-key="${id}"]`)
+const findScreenKey = (id) => document.querySelector(`div[data-key="${id}"]`)
 
 getClassName = (node) => node.getAttribute('class');
 
 addClassName = (node, currentClassName, className) => node.setAttribute('class', `${currentClassName} ${className}`);
 
 const checkKey = (event) => {
-    key = codes[event.keyCode] || null;
-    return key;
+    key = event.keyCode || null;
+    if (key !== null) return key;
 }
 
-const getNode = (keys, key) => keys.filter(element => element.innerText === key)[0]
-
 const addHighlightOnInstrument = (key) => {
-    const node =  getNode(keys, key);
-    const parentNode = getParent(node);
-    const parentClass = getClassName(parentNode);
-    addClassName(parentNode, parentClass,'playing');
+    const button = findScreenKey(key);
+    const buttonClass = getClassName(button);
+    addClassName(button, buttonClass,'playing');
 };
 
 const removeHighlightOnInstrument = (key) => {
-    const node = getNode(keys, key);
-    const parentNode = getParent(node);
-    addClassName(parentNode, '','key');
+    const button = findScreenKey(key);
+    const buttonClass = getClassName(button);
+    addClassName(button, '', 'key');
 };
 
+const playSound = (sound) => {
+    sound.currentTime = 0;
+    sound.play();
+}
+
 document.addEventListener('keydown', (event) => {
-    checkKey(event);
-    addHighlightOnInstrument(key)
+    addHighlightOnInstrument(key);
 });
 document.addEventListener('keyup', (event) => { 
-    checkKey(event);
-    removeHighlightOnInstrument(key)
+    const key = checkKey(event);
+    const sound = findAudio(key);
+    playSound(sound);
+    removeHighlightOnInstrument(key);
 });
-
-console.log(keys);
