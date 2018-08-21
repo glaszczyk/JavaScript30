826258f9b230db7be882b3e8279c7f6e58986093
glaszczyk <info@grzegorzlaszczyk.pl>
2018-08-21 23:46:57 +0200

    [js-02] Create Clock object instead of separate functions

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,45 +1,40 @@
-function init() {
-    const hands = Array.from(document.querySelectorAll('.hand'));
-    hands.forEach(h => {
-        h.style.transformOrigin = 'center right';
-        // h.style.transform='rotate(90deg)';
-    });
-};
-
-init();
-
-function newDate() {
-    const date = new Date();
-    return {
-        hours: date.getHours(),
-        minutes: date.getMinutes(),
-        seconds: date.getSeconds()
-    };
-}
-
-calculateSecondsPosition = (value) => value.seconds * 6 + 90;
-
-calculateMinutesPosition = (value) => (value.minutes * 60 + value.seconds) / 10 + 90;
-
-calculateHoursPosition = (value) => ((value.hours % 12) * 60 + value.minutes) / 2 + 90;
-
 const hoursHand = document.querySelector('.hour-hand');
 const minutesHand = document.querySelector('.min-hand');
 const secondsHand = document.querySelector('.second-hand');
 
-function add() {
-    const currentTime = newDate();
-    secondsHand.style.transform = `rotate(${calculateSecondsPosition(currentTime)}deg)`;
-    minutesHand.style.transform = `rotate(${calculateMinutesPosition(currentTime)}deg)`;
-    hoursHand.style.transform = `rotate(${calculateHoursPosition(currentTime)}deg)`;
+function AnalogClock() {
+    const date = new Date();
+    const hours = date.getHours() % 12;
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    this.getSecondsAngle = () => seconds * 6 + 90;
+    this.getMinutesAngle = () => (minutes * 60 + seconds) / 10 + 90;
+    this.getHoursAngle = () => (hours * 60 + minutes) / 2 + 90;
+}
+
+function init() {
+    const hands = Array.from(document.querySelectorAll('.hand'));
+    hands.forEach(h => {
+        h.style.transformOrigin = 'center right';
+    });
+};
+
+function displayClock() {
+    const clock = new AnalogClock();
+    secondsHand.style.transform = `rotate(${clock.getSecondsAngle()}deg)`;
+    minutesHand.style.transform = `rotate(${clock.getMinutesAngle()}deg)`;
+    hoursHand.style.transform = `rotate(${clock.getHoursAngle()}deg)`;
 }
 
 function tickTock() {
-    add(),
-        setTimeout(
-            tickTock,
-            1000
-        );
+    displayClock();
+    setTimeout(
+        tickTock,
+        1000
+    );
 }
 
-tickTock()
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+    tickTock();
+});
