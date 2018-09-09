2066fac5e6764a90295e47db11bc92a2885ec22c
glaszczyk <info@grzegorzlaszczyk.pl>
2018-09-10 00:48:54 +0200

    [js-08] Implement drawing circles. Test stroke width

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -1,13 +1,61 @@
 document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    console.log('foo');
+    const canvas = document.getElementById('draw');
+    let mouseDown = false;
+    let cursor;
+    let strokeWidth = {
+        width: 30,
+        mode: 'decrease'
+    }
+    let context;
 
     rad = (deg) => (Math.PI / 180) * deg;
 
-    const canvas = document.getElementById('draw');
+    stroke = (stroke) => {
+        switch (stroke.mode) {
+            case 'decrease': {
+                stroke.width = stroke.width - 0.2;
+                if (stroke.width<1) {
+                    stroke.mode = 'increase';
+                }
+                break;
+            }
+            case 'increase': {
+                stroke.width = stroke.width + 0.2;
+                if (stroke.width>30) {
+                    stroke.mode = 'decrease';
+                }
+            }
+        }
+        return stroke;
+    }
+    circle = (ct, {x, y}, radius) => {
+        ct.beginPath();
+        cradius = stroke(radius);
+        ct.arc(x, y, cradius.width, rad(0), rad(360));
+        ct.fill();
+    }
+
+    position = (x, y) => ({x, y});
+    
+    onMouseDown = () => {
+        mouseDown = true;
+    }
+
+    onMouseMove = (e) => {
+        if (mouseDown) {
+            cursor = position(e.x, e.y);
+            circle(context, cursor, strokeWidth);
+        }
+    }
+    
+    onMouseUp = () => {
+        mouseDown = false;
+    }
+
     if (canvas.getContext) {
-        let context = canvas.getContext('2d');
+        context = canvas.getContext('2d');
 
         context.fillStyle = 'rgb(200, 0, 0)';
         context.fillRect(100, 100, 300, 300);
@@ -23,15 +71,25 @@ function main() {
         context.beginPath();
         context.moveTo(500, 50);
         context.lineTo(600, 100);
-        context.lineTo(600, 200);
         context.lineTo(500, 200);
+        context.lineTo(600, 200);
         context.closePath();
         context.fill();
 
+        context.lineCap = 'round';
+        context.lineWidth = 10;
         context.beginPath();
         context.moveTo(400, 400);
         context.lineTo(700, 100);
+        context.stroke();
+        context.beginPath();
+        context.lineWidth = 11;
+        context.moveTo(700, 100);
         context.lineTo(700, 700);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(700, 700);
+        context.lineWidth = 12;
         context.lineTo(400, 400);
         context.stroke();
 
@@ -40,4 +98,8 @@ function main() {
         context.arc(400, 400, 100, rad(0), rad(180));
         context.stroke();
     }
+
+    canvas.addEventListener('mousedown', (event) => onMouseDown(event));
+    canvas.addEventListener('mousemove', (event) => onMouseMove(event));
+    canvas.addEventListener('mouseup', (event) => onMouseUp(event));
 }
