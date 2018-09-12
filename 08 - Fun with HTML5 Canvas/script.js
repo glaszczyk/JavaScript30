8c77a20e3c709a473972ec899a40b9ab1de7812c
glaszczyk <info@grzegorzlaszczyk.pl>
2018-09-12 22:52:46 +0200

    [js-08] Use line with varying widht

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -4,6 +4,7 @@ function main() {
     const canvas = document.getElementById('draw');
     let mouseDown = false;
     let cursor;
+    let previousCursor;
     let strokeWidth = {
         width: 30,
         mode: 'decrease'
@@ -12,7 +13,7 @@ function main() {
 
     rad = (deg) => (Math.PI / 180) * deg;
 
-    stroke = (stroke) => {
+    changeStroke = (stroke) => {
         switch (stroke.mode) {
             case 'decrease': {
                 stroke.width = stroke.width - 0.2;
@@ -30,24 +31,39 @@ function main() {
         }
         return stroke;
     }
+
     circle = (ct, {x, y}, radius) => {
         ct.beginPath();
-        cradius = stroke(radius);
+        const cradius = changeStroke(radius);
         ct.arc(x, y, cradius.width, rad(0), rad(360));
         ct.fill();
     }
 
-    position = (x, y) => ({x, y});
-    
-    onMouseDown = () => {
-        mouseDown = true;
+    drawLine = (ct, width, point1, point2) => {
+        ct.beginPath();
+        const line = changeStroke(width);
+        ct.lineCap = 'round';
+        ct.strokeStyle = 'rgb(80, 140, 200)';
+        ct.lineWidth = line.width;
+        ct.moveTo(point1.x, point1.y);
+        ct.lineTo(point2.x, point2.y);
+        ct.stroke();
     }
 
+    position = (x, y) => ({x, y});
+    
+    onMouseDown = (e) => {
+        mouseDown = true;
+        cursor = position(e.x, e.y);
+    }
+    
     onMouseMove = (e) => {
         if (mouseDown) {
+            previousCursor = {...cursor} ;
             cursor = position(e.x, e.y);
-            circle(context, cursor, strokeWidth);
+            drawLine(context, strokeWidth, previousCursor, cursor);
         }
+        
     }
     
     onMouseUp = () => {
@@ -57,46 +73,46 @@ function main() {
     if (canvas.getContext) {
         context = canvas.getContext('2d');
 
-        context.fillStyle = 'rgb(200, 0, 0)';
-        context.fillRect(100, 100, 300, 300);
+        // context.fillStyle = 'rgb(200, 0, 0)';
+        // context.fillRect(100, 100, 300, 300);
 
-        context.fillStyle = 'rgba(0, 200, 0, .5)';
-        context.fillRect(200, 200, 300, 300);
+        // context.fillStyle = 'rgba(0, 200, 0, .5)';
+        // context.fillRect(200, 200, 300, 300);
 
-        context.strokeRect(300, 300, 300, 300);
+        // context.strokeRect(300, 300, 300, 300);
         
-        context.clearRect(301, 301, 99, 99);
+        // context.clearRect(301, 301, 99, 99);
 
-        context.strokeStyle = 'rgb(80, 140, 200)';
-        context.beginPath();
-        context.moveTo(500, 50);
-        context.lineTo(600, 100);
-        context.lineTo(500, 200);
-        context.lineTo(600, 200);
-        context.closePath();
-        context.fill();
+        // context.strokeStyle = 'rgb(80, 140, 200)';
+        // context.beginPath();
+        // context.moveTo(500, 50);
+        // context.lineTo(600, 100);
+        // context.lineTo(500, 200);
+        // context.lineTo(600, 200);
+        // context.closePath();
+        // context.fill();
 
-        context.lineCap = 'round';
-        context.lineWidth = 10;
-        context.beginPath();
-        context.moveTo(400, 400);
-        context.lineTo(700, 100);
-        context.stroke();
-        context.beginPath();
-        context.lineWidth = 11;
-        context.moveTo(700, 100);
-        context.lineTo(700, 700);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(700, 700);
-        context.lineWidth = 12;
-        context.lineTo(400, 400);
-        context.stroke();
+        // context.lineCap = 'round';
+        // context.lineWidth = 10;
+        // context.beginPath();
+        // context.moveTo(400, 400);
+        // context.lineTo(700, 100);
+        // context.stroke();
+        // context.beginPath();
+        // context.lineWidth = 11;
+        // context.moveTo(700, 100);
+        // context.lineTo(700, 700);
+        // context.stroke();
+        // context.beginPath();
+        // context.moveTo(700, 700);
+        // context.lineWidth = 12;
+        // context.lineTo(400, 400);
+        // context.stroke();
 
-        context.beginPath();
-        context.moveTo(400, 400);
-        context.arc(400, 400, 100, rad(0), rad(180));
-        context.stroke();
+        // context.beginPath();
+        // context.moveTo(400, 400);
+        // context.arc(400, 400, 100, rad(0), rad(180));
+        // context.stroke();
     }
 
     canvas.addEventListener('mousedown', (event) => onMouseDown(event));
