84b71afb99e5cfa0b268fc167ca529c0b896ddbe
glaszczyk <info@grzegorzlaszczyk.pl>
2018-09-12 22:53:59 +0200

    [js-08] Remove unnecessary code

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -72,47 +72,6 @@ function main() {
 
     if (canvas.getContext) {
         context = canvas.getContext('2d');
-
-        // context.fillStyle = 'rgb(200, 0, 0)';
-        // context.fillRect(100, 100, 300, 300);
-
-        // context.fillStyle = 'rgba(0, 200, 0, .5)';
-        // context.fillRect(200, 200, 300, 300);
-
-        // context.strokeRect(300, 300, 300, 300);
-        
-        // context.clearRect(301, 301, 99, 99);
-
-        // context.strokeStyle = 'rgb(80, 140, 200)';
-        // context.beginPath();
-        // context.moveTo(500, 50);
-        // context.lineTo(600, 100);
-        // context.lineTo(500, 200);
-        // context.lineTo(600, 200);
-        // context.closePath();
-        // context.fill();
-
-        // context.lineCap = 'round';
-        // context.lineWidth = 10;
-        // context.beginPath();
-        // context.moveTo(400, 400);
-        // context.lineTo(700, 100);
-        // context.stroke();
-        // context.beginPath();
-        // context.lineWidth = 11;
-        // context.moveTo(700, 100);
-        // context.lineTo(700, 700);
-        // context.stroke();
-        // context.beginPath();
-        // context.moveTo(700, 700);
-        // context.lineWidth = 12;
-        // context.lineTo(400, 400);
-        // context.stroke();
-
-        // context.beginPath();
-        // context.moveTo(400, 400);
-        // context.arc(400, 400, 100, rad(0), rad(180));
-        // context.stroke();
     }
 
     canvas.addEventListener('mousedown', (event) => onMouseDown(event));
