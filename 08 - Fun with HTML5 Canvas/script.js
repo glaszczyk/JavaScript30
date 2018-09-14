edd9cb71be1a08b047932e57797a63ce1fb4bf1d
glaszczyk <info@grzegorzlaszczyk.pl>
2018-09-14 23:56:55 +0200

    [js-08] Use HSL model instead of RGB

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -5,11 +5,7 @@ function main() {
     let mouseDown = false;
     let cursor;
     let previousCursor;
-    let color = {
-        red: 255,
-        green: 0,
-        blue: 0
-    };
+    let colorDegree = 0;
     let speed = 1;
     let strokeWidth = {
         width: 30,
@@ -44,32 +40,23 @@ function main() {
         ct.arc(x, y, cradius.width, rad(0), rad(360));
         ct.fill();
     }
-    increaseChannel = (value, speed) => (value + speed >= 255) ? 255 : value + speed;
 
-    decreaseChannel = (value, speed) => (value - speed <= 0) ? 0 : value - speed;
-    
-    colorRGB = ({red, green, blue}, speed) => {
-        if (red === 255 && green < 255 && blue === 0) { green = increaseChannel(green, speed)}
-        else if (red > 0 && green === 255 && blue === 0) { red = decreaseChannel(red, speed) }
-        else if (red === 0 && green === 255 && blue < 255) {blue = increaseChannel(blue, speed)}
-        else if (red === 0 && green > 0 && blue === 255) {green = decreaseChannel(green, speed)}
-        else if (red < 255 && green === 0 && blue === 255 ) {red = increaseChannel(red, speed)}
-        else if (red === 255 && green === 0 && blue > 0 ) {blue = decreaseChannel(blue, speed)};
-        color = { red, green, blue};
-        return `rgb(${red}, ${green}, ${blue})`;
+    colorHSL = (degree, speed) => {
+        colorDegree = (degree % 360) + speed;
+        return `hsl(${colorDegree}, 100%, 50%)`;
     }
-    
+
     calculateSpeed = (point1, point2) => {
         const s = Math.floor(Math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2));
-        return (s>10) ? 10 : s;
+        return (s>2) ? 2 : s;
     }
 
-    drawLine = (ct, rgbColor, width, point1, point2) => {
+    drawLine = (ct, degree, width, point1, point2) => {
         ct.beginPath();
         const line = changeStroke(width);
         ct.lineCap = 'round';
         speed = calculateSpeed(point1, point2);
-        ct.strokeStyle = colorRGB(rgbColor, speed);
+        ct.strokeStyle = colorHSL(degree, speed);
         ct.lineWidth = line.width;
         ct.moveTo(point1.x, point1.y);
         ct.lineTo(point2.x, point2.y);
@@ -87,7 +74,7 @@ function main() {
         if (mouseDown) {
             previousCursor = {...cursor} ;
             cursor = position(e.x, e.y);
-            drawLine(context, color, strokeWidth, previousCursor, cursor);
+            drawLine(context, colorDegree, strokeWidth, previousCursor, cursor);
         }
         
     }
